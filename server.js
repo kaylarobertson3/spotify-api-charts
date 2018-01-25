4bb56201ba35232d28ff1e78338ff886e9440012
kaylarobertson3 <kayla.robertson3@gmail.com>
2018-01-25 17:54:31 +0100

    final touches, minus bubble chart

--- server.js
+++ server.js
@@ -10,13 +10,6 @@ app.use(bodyParser.json());
 
 // Static
 app.use('/public', express.static(__dirname + '/public'));
-// app.use(express.static('public'));
-
-
-// if app is in production (heroku), use this
-// if (process.env.NODE_ENV != 'production') {
-//     app.use('/bundle.js', require('http-proxy-middleware')({target: 'http://localhost:8081/'}));
-// }
 
 // END SETUP =================================================
 
@@ -28,8 +21,9 @@ app.get('/callback', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-// app.listen(8888, () => {
-//     console.log("I'm listening on 8080.");
-// });
 
-app.listen(process.env.PORT || 8888);
+// START SERVER ====================================================
+
+app.listen(process.env.PORT || 8888, function() {
+    console.log("I'm listening");
+});
