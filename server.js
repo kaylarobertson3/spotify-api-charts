eb6f00cd5d1626fd9b29efed2972fb2ec2ffda02
kaylarobertson3 <kayla.robertson3@gmail.com>
2018-01-25 17:26:24 +0100

    heroku

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Static
-// app.use('/public', express.static(__dirname + '/public'));
-app.use(express.static('public'));
+app.use('/public', express.static(__dirname + '/public'));
+// app.use(express.static('public'));
 
 
 // if app is in production (heroku), use this
-if (process.env.NODE_ENV != 'production') {
-    app.use('/bundle.js', require('http-proxy-middleware')({target: 'http://localhost:8081/'}));
-}
+// if (process.env.NODE_ENV != 'production') {
+//     app.use('/bundle.js', require('http-proxy-middleware')({target: 'http://localhost:8081/'}));
+// }
 
 // END SETUP =================================================
 
@@ -32,4 +32,4 @@ app.get('/callback', function (req, res) {
 //     console.log("I'm listening on 8080.");
 // });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8888);
